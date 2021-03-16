456367a4a4247152dc2f69b141ba041eae54a6c8
Noah Bennett <nbennett.320@gmail.com>
2021-03-16 13:37:33 -0400

    page setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import { NavigationBar } from './components'
+import { UploadImage } from './views'
 import './styles/App.css'
 
+// poor man's view router
+const useCurrentView = (view, props) => {
+  switch(view) {
+    case 'upload-image':
+      return <UploadImage {...props} />
+    case 'encode-image':
+      break
+    case 'decode-image':
+      break
+    default:
+      return <UploadImage {...props} />
+  }
+}
+
 const App = () => {
+  const [view, setView] = React.useState()
+  const CurrentView = props => useCurrentView(view, props)
   return (
-    <div className="page">
+    <div className="app">
       <NavigationBar />
-      
+      <CurrentView 
+        demoProp="yooo"
+      />
     </div>
   )
 }
---
+++ src/views/upload-image/UploadImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UploadImage = props => {
+  return (
+    <div className="view">
+      Upload image {props.demoProp}
+    </div>
+  )
+}
+
+export default UploadImage
