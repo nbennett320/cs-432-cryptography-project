d3be1cc55c40438a0b2427c109ce5fc2ebdfdc73
Noah Bennett <nbennett.320@gmail.com>
2021-04-20 18:01:10 -0400

    fix one digit hex colors

--- src/util/Pixel.js
+++ src/util/Pixel.js
@@ -12,7 +12,10 @@ class Pixel {
     this.rgba.g = data[1]
     this.rgba.b = data[2]
     this.rgba.a = data[3] / 255
-    this.hex = `#${data[0].toString(16)}${data[1].toString(16)}${data[2].toString(16)}`
+    const hexR = this.#_dec2HexCSubstr(data[0])
+    const hexG = this.#_dec2HexCSubstr(data[0])
+    const hexB = this.#_dec2HexCSubstr(data[0])
+    this.hex = '#' + hexR + hexG + hexB
   }
 
   rgba = { 
@@ -23,6 +26,23 @@ class Pixel {
   }
 
   hex = ""
+
+  // private
+
+  /**
+   * Convert decimal integers to a 2-character string, which 
+   * is a substring of the color hex value
+   * @param {Number} dec base 10 number
+   * @returns {String} string of hex number
+   */
+  #_dec2HexCSubstr = dec => {
+    const hexStr = dec.toString(16)
+    if(hexStr.length > 1) {
+      return hexStr
+    } else {
+      return `0${hexStr}`
+    }
+  }
 }
 
 export default Pixel
